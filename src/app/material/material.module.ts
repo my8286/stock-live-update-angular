ead2fc328414102ed327fb8d99b100825582bf64
Manish Yadav <manishyadav8286@gmail.com>
2021-09-03 19:02:33 +0530

    updated gui

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 const MaterialComponent=[
   MatButtonModule,
-  MatTableModule
+  MatTableModule,
+  MatCardModule,
+  MatToolbarModule,
+  MatIconModule
 ];
 
 @NgModule({
